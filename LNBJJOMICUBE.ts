8ab65c5eb082e91ef20dff8f7ee45585903d3c0e
LNBEDU <61104888+LNBEDU@users.noreply.github.com>
2026-03-20 19:05:40 +0900

    Add files via upload

--- LNBJJOMICUBE.ts
+++ LNBJJOMICUBE.ts
@@ -17,6 +17,8 @@ namespace JJOMICUBE {
         // TFTGraph 패키지 내부에 layoutCommon을 export로 수정하거나 
         // 여기서 직접 초기화 로직을 수행합니다.
         TFTGraph.drawStatus("System Ready", Color.Green);
+        RBTFT20.init()
+        RBTFT20.clearScreen()
         
         // 2. 네오픽셀 초기화
         JJONeo.init(12, 40);
